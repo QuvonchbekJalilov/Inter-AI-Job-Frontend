127cdd4d743f28f6e5f3bf6dacc9533847ab245d
Ismoil Dev <ismoil3436@gmail.com>
2025-09-29 17:56:28 +0500

    Vacancy integration

--- src/app/Page.vue
+++ src/app/Page.vue
@@ -23,24 +23,15 @@ import {getCurrentInstance, onMounted} from "vue";
 import axios from "axios";
 const { proxy } = getCurrentInstance()
 onMounted(() => {
-  const track = axios.get(proxy.$locale + "/v1/visits/track");
-  console.log(track)
-  const storage = localStorage.getItem("token")
-      ? localStorage
-      : sessionStorage;
-
-  const expiresAt = storage.getItem("expires_at");
-  if (expiresAt) {
-    const expireTime = new Date(expiresAt).getTime();
-    const now = Date.now();
-
-    if (now >= expireTime) {
-      storage.removeItem("token");
-      storage.removeItem("user");
-      storage.removeItem("expires_at");
-      router.push({ name: "login" });
+  const token = localStorage.getItem("token") || sessionStorage.getItem("token")
+  const track = axios.get(proxy.$locale + "/v1/visits/track", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json"
     }
-  }
+  });
+  console.log(track)
 });
 
 </script>
