d94f4a05359a3cb0eef394b6d06460f85a342401
Ismoil Dev <ismoil3436@gmail.com>
2025-10-15 11:16:14 +0500

    Vacancy integration

--- src/app/Profile.vue
+++ src/app/Profile.vue
@@ -174,7 +174,6 @@
               <!-- Input -->
               <input
                   type="number"
-                  v-model.number="limit"
                   class="w-full px-3 py-2 border rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none"
               />
 
