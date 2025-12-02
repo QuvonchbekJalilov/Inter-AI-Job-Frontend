4d41cd63b7612df9c53b363c3030e7d555abb349
Ismoil Dev <ismoil3436@gmail.com>
2025-12-02 13:15:48 +0500

    Tracking

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import Vue3Toastify from "vue3-toastify"
 
 const app = createApp(App)
 app.use(Vue3Toastify, { autoClose: 4000 })
-app.config.globalProperties.$locale = "https://api.inter-ai.uz/api"
+app.config.globalProperties.$locale = "http://127.0.0.1:8000/api"
 app.config.globalProperties.$production = "https://api.inter-ai.uz/api"
 app.use(router)
 app.mount('#app')
